20f485ff7a776ad7aa6ce0f94c9b716e6ef1e8c7
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-05-04 18:36:04 +0300

    home work 9 first not working version

--- code.js
+++ code.js
@@ -1,42 +1,50 @@
 const buttonAdd = document.querySelector("#addNewToDO");
+const formListToDo = document.querySelector('#form_list_to_do');
+const listToDo = document.querySelector('.listToDo');
+const input = document.querySelector('#newToDo');
+const newToDo = document.querySelector('#newToDo').value;
 
-document.querySelector('#form_list_to_do').onsubmit =
+formListToDo.onsubmit =
     function(e){
         e.preventDefault();
     };
 
-buttonAdd.addEventListener('click', function  formValidation(){
+input.addEventListener('change', function validation(){
     const errorMessage = document.querySelector('.error_message');
-    if (document.querySelector('#newToDo').checkValidity()) {
+    const regExp =  /^[a-zA-Z0-9]{2,25}$/g;
+    if (regExp.test(newToDo)){
         console.log(true);
-        const newToDo = document.querySelector('#newToDo').value;
-        let li = document.createElement('li');
-        li.innerHTML = newToDo;
-        document.querySelector('ul').append(li);
-        document.querySelector('#newToDo').value = " ";
-
-        addButton()
+        input.classList.remove('invalid');
         errorMessage.innerText = " ";
+        return true;
     }
-    else{
+    else {
         console.log(false);
+        input.classList.add('invalid');
         errorMessage.innerText = 'Your text is invalid. Please use only latin letters and numbers. You task can not be shorter that 2 and longer that 25 characters';
-        }
+        return false;
+    }
+})
+
+buttonAdd.addEventListener('click', function (){
+    //const newToDo = document.querySelector('#newToDo').value;
+    //const regExp = /^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$/;
+    const regExp =  /^[a-zA-Z0-9]{2,25}$/g;
+    if (regExp.test(newToDo)){
+        createNewLi()
+        addButton()
+
+    }
 
 })
 
-
-const listToDo = document.querySelector('.listToDo');
-listToDo.addEventListener( 'click', function(event){
-    if (event.target.closest('li')){
-        event.target.closest('li').classList.toggle('done');
-    }
-    if (event.target.closest('button')){
-
-        event.target.previousElementSibling.remove();
-        event.target.remove();
-    }
-})
+function createNewLi() {
+    const newToDo = document.querySelector('#newToDo').value;
+    let li = document.createElement('li');
+    li.innerHTML = newToDo;
+    document.querySelector('ul').append(li);
+    document.querySelector('#newToDo').value = " ";
+}
 
 function addButton (){
     let deleteButton = document.createElement('button');
@@ -45,3 +53,12 @@ function addButton (){
     document.querySelector('li:last-child').after(deleteButton);
 }
 
+listToDo.addEventListener( 'click', function(event){
+    if (event.target.closest('li')){
+        event.target.closest('li').classList.toggle('done');
+    }
+    if (event.target.closest('button')){
+        event.target.previousElementSibling.remove();
+        event.target.remove();
+    }
+})
